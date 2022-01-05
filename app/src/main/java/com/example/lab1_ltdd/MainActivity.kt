abcc1903e946fa711133b80b05203ddf747c2f4b
Henfie <96721297+Henfie@users.noreply.github.com>
2022-01-05 13:39:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab1_ltdd/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lab1_ltdd
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+            btnCong.setOnClickListener {
+                var so1 : Double = (etNumber1.text.toString()).toDouble()
+                var so2 : Double = (etNumber2.text.toString()).toDouble()
+                var kq : Double = so1 + so2
+                textKetQua.text = kq.toString()
+            }
+    }
+}
---
+++ app/src/main/java/com/example/lab1_ltdd/Xinchao.kt
@@ -0,0 +1,6 @@
+package com.example.lab1_ltdd
+
+fun main(){
+    println("Sawadi khap")
+    println("Nanon Korapat")
+}
